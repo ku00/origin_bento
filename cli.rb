91e56d66b2629301bfef016ebe41cfbfdaa38001
takuminnnn <skentaro36@gmail.com>
2014-05-08 12:37:51 +0000

    タブをスペースに

--- cli.rb
+++ cli.rb
@@ -4,12 +4,12 @@ require 'thor'
 require 'open3'
  
 class Order < Thor
-	default_command :git_pull
+    default_command :git_pull
 
-	desc "git_pull", ""
-	def git_pull
-		puts "sorry... Failed to order origin-bento. (´･_･`) "
-	end
+    desc "git_pull", ""
+    def git_pull
+        puts "sorry... Failed to order origin-bento. (´･_･`) "
+    end
 
     desc "origin_bento", "Order origin_bento by casperjs"
     def origin_bento
